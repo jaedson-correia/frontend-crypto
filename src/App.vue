40eab92c8f7e8701a9ceeffd8b568dd036dca277
Jaedson <jaedson17@live.com>
2022-08-24 23:39:03 -0300

    removing bugged footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script>
 import Navbar from "./main/Navbar.vue";
-import Footer from "./main/Footer.vue";
 import LastPrice from "./main/LastPrice.vue";
 import DateTimePrice from "./main/DateTimePrice.vue";
 
 export default {
-	components: { Navbar, Footer, LastPrice, DateTimePrice }
+	components: { Navbar, LastPrice, DateTimePrice }
 }
 </script>
 
@@ -14,13 +13,9 @@ export default {
 		<Navbar></Navbar>
 	</header>
 
-	<main class="flex flex-col gap-4 pb-8">
+	<main class="flex flex-col gap-4">
 		<LastPrice></LastPrice>
 		<DateTimePrice></DateTimePrice>
 	</main>
-
-	<footer>
-		<Footer></Footer>
-	</footer>
 
 </template>
--- src/main/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="flex w-full justify-center md:absolute p-1 bottom-0 h-8 border-t-2">
-        <h1 class="text-white">Made by: <a target="_blank" href="https://www.linkedin.com/in/jaedson-correia-da-costa-3b0ba5209/">Jaedson</a></h1>
-    </div>
-</template>

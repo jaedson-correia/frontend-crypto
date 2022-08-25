35fa533950f9b531d2c88714c14facdd9a683148
Jaedson <jaedson17@live.com>
2022-08-24 22:42:07 -0300

    prep for github pages

--- src/main/Navbar.vue
+++ src/main/Navbar.vue
@@ -5,7 +5,7 @@
             <h2 class="text-white">
                 <span class="hidden lg:inline">Powered By </span><a target="_blank" href="https://www.coingecko.com/">CoinGecko</a>
             </h2>
-            <img width="24" src="../../coingecko.svg" alt="Coin Gecko Logo" />
+            <img width="24" src="../../public/coingecko.svg" alt="Coin Gecko Logo" />
         </div>
     </div>
 </template>
